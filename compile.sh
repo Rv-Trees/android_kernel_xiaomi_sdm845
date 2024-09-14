154171719aaf7c66af6f3ffe5995a6c6190b6f61
Rve <rve27github@gmail.com>
2024-09-15 06:41:35 +0700

    compile: Clean up and simplify
    Signed-off-by: Rve <rve27github@gmail.com>

--- compile.sh
+++ compile.sh
@@ -6,7 +6,6 @@ CLANGDIR="/home/rve/RvClang"
 rm -rf out/compile.log
 
 mkdir -p out
-mkdir out/RvKernel
 
 export KBUILD_BUILD_USER=Radika
 export KBUILD_BUILD_HOST=Rve
@@ -14,15 +13,6 @@ export USE_CCACHE=1
 export PATH="$CLANGDIR/bin:$PATH"
 
 make O=out ARCH=arm64 $DEFCONFIG
-
-MAKE="./makeparallel"
-
-START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
 
 rve () {
 make -j$(nproc --all) O=out LLVM=1 LLVM_IAS=1 \
@@ -45,14 +35,3 @@ CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
 
 rve 2>&1 | tee -a out/compile.log
-if [ $? -ne 0 ]
-then
-    echo "Build failed"
-else
-    echo "Build succesful"
-    cp out/arch/arm64/boot/Image.gz-dtb out/RvKernel/Image.gz-dtb
-fi
-
-END=$(date +"%s")
-DIFF=$(($END - $START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
