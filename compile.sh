8e486700ffc91eb7d7f2521a7fa468c28d174e0c
Rve27 <rve27github@gmail.com>
2025-01-02 23:29:41 +0800

    compile: Add options to use KernelSU or not
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,66 +1,93 @@
 #!/bin/sh
 #
 # Compile script kernel
-# Copyright (C) 2024 Rve.
+# Copyright (C) 2024-2025 Rve.
 
-show_help() {
-    echo "Usage: $0 [options]"
+first_help() {
+    echo "Usage: $0 [device] [options]"
     echo
-    echo "Options:"
+    echo "Device:"
     echo "  --beryllium    Compile kernel for beryllium (POCO F1)"
     echo "  --dipper       Compile kernel for dipper (Mi 8)"
     echo "  --equuleus     Compile kernel for equuleus (Mi 8 Pro)"
     echo "  --perseus      Compile kernel for perseus (Mi Mix 3)"
     echo "  --polaris      Compile kernel for polaris (Mi Mix 2S)"
     echo "  --ursa         Compile kernel for ursa (Mi 8 Explorer Edition)"
+    echo
+    echo "  --help         Display this help message"
+    echo
+    exit 1
+}
+
+second_help() {
+    echo "Usage: $0 [device] [options]"
+    echo
+    echo "Options:"
+    echo "  --ksu          with KernelSU"
+    echo "  --no-ksu       without KernelSU"
+    echo
     echo "  --help         Display this help message"
     echo
     exit 1
 }
 
 if [ $# -eq 0 ]; then
-    show_help
+    first_help
 fi
 
 case "$1" in
     --beryllium)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/beryllium.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="beryllium.config"
         ;;
     --dipper)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/dipper.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="dipper.config"
         ;;
     --equuleus)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/equuleus.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="equuleus.config"
         ;;
     --perseus)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/perseus.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="perseus.config"
         ;;
     --polaris)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/polaris.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="polaris.config"
         ;;
     --ursa)
-        cat arch/arm64/configs/vendor/xiaomi/mi845_defconfig \
-            arch/arm64/configs/vendor/xiaomi/ursa.config \
-            > arch/arm64/configs/generated_defconfig
+        DEVICE_CONFIG="ursa.config"
         ;;
     --help)
-        show_help
+        first_help
         ;;
     *)
-        echo "Error: Unknown option '$1'"
-        show_help
+        echo "Error: Unknown device '$1'"
+        first_help
         ;;
 esac
+
+if [ $# -lt 2 ]; then
+    second_help
+fi
+
+case "$2" in
+    --ksu)
+        CONFIG="mi845-ksu_defconfig"
+	OUTPUT_DIR="out/RvKernel/ksu"
+        ;;
+    --no-ksu)
+        CONFIG="mi845_defconfig"
+	OUTPUT_DIR="out/RvKernel/non-ksu"
+        ;;
+    --help)
+        second_help
+        ;;
+    *)
+        echo "Error: Unknown option '$2'"
+        second_help
+        ;;
+esac
+
+cat arch/arm64/configs/vendor/xiaomi/$CONFIG \
+    arch/arm64/configs/vendor/xiaomi/$DEVICE_CONFIG \
+    > arch/arm64/configs/generated_defconfig
 
 DEFCONFIG="generated_defconfig"
 CLANGDIR="/home/rve/RvClang"
@@ -75,8 +102,10 @@ export PATH="$CLANGDIR/bin:$PATH"
 
 make O=out ARCH=arm64 $DEFCONFIG
 
+start_time=$(date +%s)
+
 rve () {
-make -j$(nproc --all) O=out LLVM=1 LLVM_IAS=1 \
+make -j$(nproc --all) O=out LLVM=1 \
 ARCH=arm64 \
 CC="ccache clang" \
 LD=ld.lld \
@@ -96,3 +125,17 @@ CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
 
 rve 2>&1 | tee -a out/compile.log
+
+end_time=$(date +%s)
+compile_time=$((end_time - start_time))
+
+if [ $compile_time -ge 60 ]; then
+    compile_time_min=$((compile_time / 60))
+    compile_time_sec=$((compile_time % 60))
+    echo -e "\033[1;33mCompile time: $compile_time_min minutes and $compile_time_sec seconds\033[0m"
+else
+    echo -e "\033[1;33mCompile time: $compile_time seconds\033[0m"
+fi
+
+mkdir -p $OUTPUT_DIR
+mv out/arch/arm64/boot/Image.gz-dtb $OUTPUT_DIR/
